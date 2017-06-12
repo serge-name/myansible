f6b8b126e33a4ffc0d6c5a55634c7bb94c6b190f
Serge Pokhodyaev <snp@z9.ru>
2017-06-12 19:11:30 +0300

    add mydummy module

---
+++ library/mydummy.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# coding: utf-8
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            msg         = dict(type='str',  required=False, default=""),
+            changed     = dict(type='bool', required=False, default=False),
+            failed      = dict(type='bool', required=False, default=False),
+        )
+    )
+
+    msg = module.params['msg']
+    changed = module.params['changed']
+    failed = module.params['failed']
+
+    module.exit_json(msg=msg, changed=changed, failed=failed)
+
+from ansible.module_utils.basic import *
+main()
