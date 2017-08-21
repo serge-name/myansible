8be0f6703bdd95df9b6a88c7abb8e68f9929122b
Serge Pokhodyaev <snp@z9.ru>
2017-08-21 21:47:05 +0300

    add 'is_empty_or_undefined' filter

--- filter_plugins/is_empty.py
+++ filter_plugins/is_empty.py
@@ -1,13 +1,17 @@
+from jinja2.runtime import Undefined, StrictUndefined
+
 class FilterModule(object):
     ''' A comment '''
 
     def filters(self):
         return {
             'is_empty': self.is_empty,
+            'is_empty_or_undefined': self.is_empty_or_undefined,
         }
 
     def is_empty(self, input_value):
-        if input_value is None or len(input_value) == 0:
-            return True
-        else:
-            return False
+        return (input_value is None or len(input_value) == 0)
+
+    def is_empty_or_undefined(self, input_value):
+        return (type(input_value) in (StrictUndefined, Undefined, ) or
+                input_value is None or len(input_value) == 0)
